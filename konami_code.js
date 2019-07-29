922ab6158c132fac4413a5739069fd0633ff9758
Brian Simonton <bsimonton78@gmail.com>
2019-07-29 12:45:17 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,22 @@ function init() {
   // Write your JavaScript code inside the init() function
   
   
-  
+  document.body.addEventListener('keydown', function(e) {
+		const key = parseInt(e.detail || e.which);
+
+		if (key === code[index]) {
+			index++;
+			if (index === code.length) {
+				alert("Yay!");
+				index = 0;
+			}
+		} else {
+			index = 0;
+		}
+	});
+	
+
+
   
   
   
